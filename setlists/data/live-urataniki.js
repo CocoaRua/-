edb2e095db531c75591229708294c419111692d4
CocoaRua <cocoa1003.rabbit@gmail.com>
2026-09-06 13:58:20 +0900

    Update live-urataniki.js

--- setlists/data/live-urataniki.js
+++ setlists/data/live-urataniki.js
@@ -1,1 +1,270 @@
+addLiveTour(
+  "うらたぬき",
+  "2025",
+  "Uratanuki Birthday Live 2025 -Update.-",
+  [
+{
+  date: "8月6日",
+  place: "宮城・仙台サンプラザホール",
+  songs: [
 
+  ]
+},
+{
+  date: "8月8日",
+  place: "愛知・Niterra日本特殊陶業市民会館フォレストホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月9日",
+  place: "大阪・グランキューブ大阪",
+  songs: [
+
+  ]
+},
+{
+  date: "8月16日",
+  place: "東京国際フォーラム ホールA",
+  songs: [
+
+  ]
+},
+{
+  date: "8月17日",
+  place: "静岡・富士市文化会館ロゼシアター大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月22日",
+  place: "広島・JMSアステールプラザ 大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月24日",
+  place: "福岡・久留米シティプラザ ザ・グランドホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月26日",
+  place: "埼玉・大宮ソニックシティ 大ホール",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+addLiveTour(
+  "うらたぬき",
+  "2024",
+  "Uratanuki Birthday Live 2024 -JEWEL-",
+  [
+{
+  date: "8月7日",
+  place: "宮城・東京エレクトロンホール宮城",
+  songs: [
+
+  ]
+},
+{
+  date: "8月9日",
+  place: "神奈川・パシフィコ横浜 国立大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月11日",
+  place: "愛知・Niterra日本特殊陶業市民会館フォレストホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月12日",
+  place: "大阪・フェニーチェ堺",
+  songs: [
+
+  ]
+},
+{
+  date: "8月16日",
+  place: "静岡・静岡市清水文化会館マリナート大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月18日",
+  place: "福岡・北九州ソレイユホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月20日",
+  place: "埼玉・大宮ソニックシティ大ホール",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+addLiveTour(
+  "うらたぬき",
+  "2023",
+  "Uratanuki Birthday Live 2023 ～Flower～",
+  [
+{
+  date: "8月4日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "8月7日",
+  place: "宮城・SENDAI GIGS",
+  songs: [
+
+  ]
+},
+{
+  date: "8月9日",
+  place: "東京・Zepp Haneda(TOKYO)",
+  songs: [
+
+  ]
+},
+{
+  date: "8月17日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "8月23日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+
+
+addLiveTour(
+  "うらたぬき",
+  "2022",
+  "Uratanuki Birthday Live ～Love!Love!～",
+  [
+{
+  date: "8月8日",
+  place: "愛知・愛知県芸術劇場 大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "8月9日",
+  place: "東京・LINE CUBE SHIBUYA",
+  songs: [
+
+  ]
+},
+{
+  date: "8月28日",
+  place: "大阪・フェニーチェ堺",
+  songs: [
+
+  ]
+},
+{
+  date: "8月31日",
+  place: "宮城・仙台サンプラザホール",
+  songs: [
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "うらたぬき",
+  "2021",
+  "Uratanuki Birthday Online Live ～date.～",
+  [
+{
+  date: "8月9日",
+  place: "ONLINE LIVE（無観客配信ライブ）",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "うらたぬき",
+  "2019",
+  "Uratanuki Birthday Live～Unlimited～",
+  [
+{
+  date: "8月8日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "8月9日",
+  place: "大阪・Zepp Namba",
+  songs: [
+
+  ]
+},
+{
+  date: "8月12日",
+  place: "東京・Zepp Tokyo",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "うらたぬき",
+  "2017",
+  "Uratanuki Birthday Live～cute style&cool style～",
+  [
+{
+  date: "8月17日",
+  place: "東京・渋谷 TSUTAYA O-EAST",
+  songs: [
+
+  ]
+    }
+  ]
+);
