dd74a739de1588590b4a86a9c2f5fe2d207780a1
CocoaRua <cocoa1003.rabbit@gmail.com>
2026-09-06 13:57:06 +0900

    Update live-sakata.js

--- setlists/data/live-sakata.js
+++ setlists/data/live-sakata.js
@@ -1,1 +1,374 @@
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2026",
+  "AHO NO SAKATA LIVE TOUR 2026 -STAR [T] RAIL-",
+  [
+{
+  date: "6月6日",
+  place: "宮城・仙台サンプラザホール",
+  songs: [
 
+  ]
+},
+{
+  date: "6月10日",
+  place: "福岡・福岡市民ホール 大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月13日",
+  place: "兵庫・神戸国際会館こくさいホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月19日",
+  place: "愛知・Niterra日本特殊陶業市民会館フォレストホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月20日",
+  place: "大阪・大阪国際会議場（グランキューブ大阪）メインホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月27日",
+  place: "東京・両国国技館",
+  songs: [
+
+  ]
+},
+{
+  date: "6月28日",
+  place: "東京・両国国技館",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2025",
+  "AHO NO SAKATA LIVE TOUR 2025 -Nurse-",
+  [
+{
+  date: "6月3日",
+  place: "愛知・Niterra日本特殊陶業市民会館フォレストホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月6日",
+  place: "兵庫・神戸国際会館こくさいホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月10日",
+  place: "福岡・福岡市民ホール 大ホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月14日",
+  place: "宮城・仙台サンプラザホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月21日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "6月28日",
+  place: "千葉・幕張メッセ国際展示場4・5・6ホール",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2024",
+  "AHO NO SAKATA 15th ANNIVERSARY TOUR -HERO-",
+  [
+{
+  date: "6月13日",
+  place: "大阪・大阪国際会議場（グランキューブ大阪）メインホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月16日",
+  place: "福岡・北九州ソレイユホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月21日",
+  place: "愛知・Niterra日本特殊陶業市民会館フォレストホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月22日",
+  place: "兵庫・神戸国際会館こくさいホール",
+  songs: [
+
+  ]
+},
+{
+  date: "6月30日",
+  place: "東京・日本武道館",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2023",
+  "AHO NO SAKATA LIVE TOUR 2023 -Home-",
+  [
+{
+  date: "6月10日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "6月11日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "6月13日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "6月17日",
+  place: "宮城・SENDAI GIGS",
+  songs: [
+
+  ]
+},
+{
+  date: "6月25日",
+  place: "東京・Zepp Haneda(TOKYO)",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+
+
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2022",
+  "AHO NO SAKATA LIVE TOUR 2022 -Steal your heart-",
+  [
+{
+  date: "6月4日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "6月5日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "6月12日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "6月17日",
+  place: "北海道・Zepp Sapporo",
+  songs: [
+
+  ]
+},
+{
+  date: "6月23日",
+  place: "宮城・SENDAI GIGS",
+  songs: [
+
+  ]
+},
+{
+  date: "6月25日",
+  place: "東京・Zepp Haneda(TOKYO)",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2021",
+  "AHO NO SAKATA LIVE TOUR 2021 -Redo Parade-",
+  [
+{
+  date: "5月30日",
+  place: "東京・Zepp Haneda(TOKYO)",
+  songs: [
+
+  ]
+},
+{
+  date: "6月12日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "6月26日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "6月27日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2019",
+  "AHO NO SAKATA LIVE TOUR 2019 キミに伝えたいこと-Message for you-",
+  [
+{
+  date: "5月3日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "5月4日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "5月6日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "5月11日",
+  place: "東京・Zepp Tokyo",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2018",
+  "坂田 Second One Man Live ～Angel&Demons 来日!～",
+  [
+{
+  date: "1月7日",
+  place: "東京・Zepp Tokyo",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+
+
+
+
+addLiveTour(
+  "あほの坂田。/となりの坂田。",
+  "2018",
+  "坂田 Second One Man Live ～Angel&Demons 来日!～",
+  [
+{
+  date: "1月7日",
+  place: "東京・Zepp Tokyo",
+  songs: [
+
+  ]
+    }
+  ]
+);
