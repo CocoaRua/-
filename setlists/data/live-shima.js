0cc732a4037684fe302d64509f47e6b6ce4a1d09
CocoaRua <cocoa1003.rabbit@gmail.com>
2026-09-06 13:57:58 +0900

    Update live-shima.js

--- setlists/data/live-shima.js
+++ setlists/data/live-shima.js
@@ -1,1 +1,348 @@
+addLiveTour(
+  "志麻",
+  "2026",
+  "SHIMA LIVE TOUR 2026 色なき国のアリス ～Alice in colorless wonderland～",
+  [
+{
+  date: "5月8日",
+  place: "愛知・Niterra日本特殊陶業市民会館フォレストホール",
+  songs: [
 
+  ]
+},
+{
+  date: "5月10日",
+  place: "高知・新来島高知重工ホール オレンジホール(高知県立県民文化ホール)",
+  songs: [
+
+  ]
+},
+{
+  date: "5月24日",
+  place: "大阪・グランキューブ大阪",
+  songs: [
+
+  ]
+},
+{
+  date: "5月29日",
+  place: "宮城・東京エレクトロンホール宮城",
+  songs: [
+
+  ]
+},
+{
+  date: "5月31日",
+  place: "神奈川・パシフィコ横浜 国立大ホール",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+addLiveTour(
+  "志麻",
+  "2025",
+  "SHIMA LIVE TOUR 2025 -shiki-",
+  [
+{
+  date: "5月3日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "5月5日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "5月6日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "5月10日",
+  place: "宮城・SENDAI GIGS",
+  songs: [
+
+  ]
+},
+{
+  date: "5月18日",
+  place: "東京・Zepp Haneda（TOKYO）",
+  songs: [
+
+  ]
+},
+{
+  date: "5月25日",
+  place: "高知・高知県立県民文化ホール オレンジホール",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+addLiveTour(
+  "志麻",
+  "2024",
+  "SHIMA LIVE TOUR 2024 -star!-",
+  [
+{
+  date: "5月5日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "5月6日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "5月18日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "5月26日",
+  place: "東京・Zepp Haneda（TOKYO）",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+
+addLiveTour(
+  "志麻",
+  "2023",
+  "SHIMA LIVE TOUR 2023 ～Violet～",
+  [
+{
+  date: "5月5日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+},
+{
+  date: "5月6日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "5月11日",
+  place: "宮城・SENDAI GIGS",
+  songs: [
+
+  ]
+},
+{
+  date: "5月14日",
+  place: "東京・Zepp Haneda(TOKYO)",
+  songs: [
+
+  ]
+},
+{
+  date: "5月17日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "5月25日",
+  place: "高知・高知県立県民文化ホール オレンジホール",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+addLiveTour(
+  "志麻",
+  "2022",
+  "SHIMA LIVE TOUR 2022 -ミチシルベ-",
+  [
+{
+  date: "5月11日",
+  place: "大阪・Zepp Namba(OSAKA)",
+  songs: [
+
+  ]
+},
+{
+  date: "5月12日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "5月14日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "5月18日",
+  place: "北海道・Zepp Sapporo",
+  songs: [
+
+  ]
+},
+{
+  date: "5月20日",
+  place: "宮城・SENDAI GIGS",
+  songs: [
+
+  ]
+},
+{
+  date: "5月21日",
+  place: "東京・Zepp DiverCity(TOKYO)",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+addLiveTour(
+  "志麻",
+  "2021",
+  "SHIMA LIVE TOUR 2021 -re:noise-",
+  [
+{
+  date: "5月1日",
+  place: "福岡・Zepp Fukuoka",
+  songs: [
+
+  ]
+},
+{
+  date: "5月4日",
+  place: "愛知・Zepp Nagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "5月16日 昼の部",
+  place: "東京・Zepp DiverCity(TOKYO)",
+  songs: [
+
+  ]
+},
+{
+  date: "5月16日 夜の部",
+  place: "東京・Zepp DiverCity(TOKYO)",
+  songs: [
+
+  ]
+},
+{
+  date: "8月5日",
+  place: "大阪・Zepp Namba(OSAKA)",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+
+
+
+
+
+
+
+
+addLiveTour(
+  "志麻",
+  "2019",
+  "志麻ワンマンライブ First OrderⅡ～Mission to Save You～",
+  [
+{
+  date: "4月20日",
+  place: "愛知・ZeppNagoya",
+  songs: [
+
+  ]
+},
+{
+  date: "4月26日",
+  place: "東京・ZeppTokyo",
+  songs: [
+
+  ]
+},
+{
+  date: "4月27日",
+  place: "大阪・Zepp Osaka Bayside",
+  songs: [
+
+  ]
+    }
+  ]
+);
+
+
+
+
+
+
+
+
+addLiveTour(
+  "志麻",
+  "2018",
+  "志麻ワンマンライブ「First Order」",
+  [
+{
+  date: "5月4日",
+  place: "東京・新宿Reny",
+  songs: [
+
+  ]
+    }
+  ]
+);
